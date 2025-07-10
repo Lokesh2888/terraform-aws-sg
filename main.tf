92055a70da3d9beb60eccc74c8f16a78c767ed78
Lokesh <lokesh.kumar131991@gmail.com>
2025-07-10 20:32:04 +0530

    gjkl;

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "main" {
     var.sg_tags,
     local.common_tags, 
     {
-        Name = "${var.var.project}-${var.var.environment}-${var.sg_name}"
+        Name = "${var.project}-${var.environment}-${var.sg_name}"
     }
 
   )
